560bd94c92f9cddb6f64f84c6263b6f893e89cd8
Dmitriy Dudnik <poniraq@gmail.com>
2023-08-02 20:06:57 +0300

    run formatter

--- src/cli.js
+++ src/cli.js
@@ -3,16 +3,17 @@ const fs = require('fs');
 const Config = require('./config');
 
 const args = process.argv.slice(2);
-const [ input, NAME, ORGANIZATION, output ] = args;
+const [input, NAME, ORGANIZATION, output] = args;
 
 const config = Config.from_ovpn(input);
 const xml = config.to_xml({
-    NAME, ORGANIZATION,
-    pretty: true
+  NAME,
+  ORGANIZATION,
+  pretty: true
 });
 
 if (output) {
-    fs.writeFileSync(output, xml);
+  fs.writeFileSync(output, xml);
 } else {
-    console.log(xml);
+  console.log(xml);
 }
--- src/config/index.js
+++ src/config/index.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 const ovpn = require('./ovpn-parser');
 
 const from_ovpn = (filepath) => {
-    const content = fs.readFileSync(filepath).toString();
-    return ovpn.parse_config(content);
+  const content = fs.readFileSync(filepath).toString();
+  return ovpn.parse_config(content);
 };
 
 module.exports = {
-    from_ovpn
-}
+  from_ovpn
+};
